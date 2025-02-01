5c5e55f3ea71cdb85ab4a4c012d2fb226de4be06
Tibedox <tibedox@mail.ru>
2025-02-01 18:31:45 +0600

    Enemy Ship

--- core/src/main/java/ru/pifpaf/Enemy.java
+++ core/src/main/java/ru/pifpaf/Enemy.java
@@ -1,15 +1,20 @@
 package ru.pifpaf;
 
+import static ru.pifpaf.Main.SCR_HEIGHT;
+import static ru.pifpaf.Main.SCR_WIDTH;
+
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.utils.TimeUtils;
 
-public class Ship extends SpaceObject{
+public class Enemy extends SpaceObject{
     public int phase, nPhases = 12;
     private long timeLastPhase, timePhaseInterval = 33;
 
-    public Ship(float x, float y) {
+    public Enemy(float x, float y) {
         super(x, y);
         width = 200;
         height = 200;
+        spawn();
     }
 
     @Override
@@ -23,5 +28,11 @@ public class Ship extends SpaceObject{
             if (++phase == nPhases) phase = 0;
             timeLastPhase = TimeUtils.millis();
         }
+    }
+
+    private void spawn() {
+        x = MathUtils.random(0+width/2, SCR_WIDTH-width/2);
+        y = MathUtils.random(SCR_HEIGHT+height, SCR_HEIGHT*2);
+        vy = MathUtils.random(-8f, -6f);
     }
 }
